d51b5c7795dff9ab6cf7d9ad21ca7df1d7698e93
Matthew Loughnane <mat.loughnane@gmail.com>
2015-06-28 22:43:54 +0100

    optimized images, seo improvements

--- assets/js/sidepanel.js
+++ assets/js/sidepanel.js
@@ -12,3 +12,32 @@ jQuery(document).ready(function($){
 		}
 	});
 });
+
+jQuery(document).ready(function($) {
+     $.fn.doubleTap = function(doubleTapCallback) {
+         return this.each(function(){
+			var elm = this;
+			var lastTap = 0;
+			$(elm).bind('vmousedown', function (e) {
+                                var now = (new Date()).valueOf();
+				var diff = (now - lastTap);
+                    lastTap = now ;
+                        if (diff < 250) {
+                    if($.isFunction( doubleTapCallback ))
+                    {
+                       doubleTapCallback.call(elm);
+                    }
+            	}      
+			});
+         });
+    }
+});
+
+jQuery(document).ready(function($) {
+	$(".doubleTap").doubleTap(function(){
+		$( ".easter_egg" ).toggle();
+	 });
+	$( ".doubleTap" ).dblclick(function() {
+		$( ".easter_egg" ).toggle();
+	});
+});
